1b7a12d930d17ab7bb62ec516388abede3c77f7d
YusufKapukaya <62699767+YusufKapukaya@users.noreply.github.com>
2023-04-13 23:10:52 +0200

    Add files via upload

---
+++ api/routes/categories.js
@@ -0,0 +1,51 @@
+const Category = require("../models/Category.js"); 
+const express = require("express");
+const router = express.Router();
+
+//! read  
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+
+  }
+});
+
+//! create
+router.post("/add-category", async (req, res) => {
+  try {
+    const newCategory = new Category(req.body);
+    await newCategory.save();
+    res.status(200).json("Item added successfuly.");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+//! update
+router.put("/update-category", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
+    res.status(200).json("Item updated successfuly.");
+  } catch (error) {
+    res.status(500).json(error);
+
+  }
+});
+
+
+//! delete
+router.delete("/delete-category", async (req, res) => {
+  try {
+    await Category.findOneAndDelete({ _id: req.body.categoryId });
+    res.status(200).json("Item deleted successfuly.");
+  } catch (error) {
+    res.status(500).json(error);
+
+  }
+});
+
+module.exports = router;
